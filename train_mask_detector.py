079be3e371e3b9e852fa28f00dbda974001afe49
ritvikb99 <ritvikb99@gmail.com>
2020-12-01 00:22:12 +0530

    Fixed argument parsing error

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -23,7 +23,7 @@ import os
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True, help="path to input database")
 ap.add_argument("-p", "--plot", type=str, default="plot.png", help="path to output loss/accuracy graph")
-ap.add_argument("-d", "--dataset", type=str, default="mask_detector.model", help="path to output face mask detector model")
+ap.add_argument("-m", "--model", type=str, default="mask_detector.model", help="path to output face mask detector model")
 args = vars(ap.parse_args())
 
 # initialize the initial learning rate, number of epochs to train for,
