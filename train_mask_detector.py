22249901b14a78ed3246dad44730fc0a54dc88aa
ritvikb99 <ritvikb99@gmail.com>
2020-12-01 00:01:13 +0530

    Added face_detector model files and dataset

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -117,14 +117,14 @@ predIdxs = model.predict(testX, batch_size=BS)
 # for each image in the testing set we need to find the index of the
 # label with corresponding largest predicted probability
 predIdxs = np.argmax(predIdxs, axis=1)
-# show a nicely formatted classification report
+# show classification report
 print(classification_report(testY.argmax(axis=1), predIdxs,
 	target_names=lb.classes_))
 # serialize the model to disk
 print("[INFO] saving mask detector model...")
 model.save(args["model"], save_format="h5")
 
-# plot the training loss and accuracy
+# plotting the training loss and accuracy
 N = EPOCHS
 plt.style.use("ggplot")
 plt.figure()
